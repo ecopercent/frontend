27d7f4ee97ecf22aa9f89adbab9381dfb62da621
sumnii <sumsong@student.42seoul.kr>
2023-02-28 14:45:03 +0900

    feat: 프로필 메시지 단락 추가
    - How : 유저 데이터의 내용 삽입
    - ECOPERCENT-180

--- src/profile.js
+++ src/profile.js
@@ -4,11 +4,20 @@ export default function Profile({ user }) {
     <>
       <ProfileImage src={user.profile_image} />
       {/* 프로필 편집 버튼 */}
-      {/* 텍스트 단락 */}
+      <ProfileMessage nickname={user.nickname} text={user.profile_message} />
     </>
   );
 }
 
 function ProfileImage({ src }) {
   return <img src={src} alt="user_profile_image" />;
+}
+
+function ProfileMessage({ nickname, text }) {
+  return (
+    <>
+      <p>{nickname}</p>
+      <p>{text}</p>
+    </>
+  );
 }
