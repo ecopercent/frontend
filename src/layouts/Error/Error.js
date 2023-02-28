9576489ca3873c4a3beae48be91fd2ede063a023
hyopark <hyopark@hyoparkui-MacBookAir.local>
2023-02-28 17:48:34 +0900

    feat : cra으로 리액트 앱 생성 후 기본 레이아웃 구성과 폴더 구조 정립
    -cra로 리액트앱 생성
    -로그인, 홈, 에러 레이아웃 구분
    -소스, 컴포넌트, 레이아웃 디렉토리 생성

---
+++ src/layouts/Error/Error.js
@@ -0,0 +1,10 @@
+import React  from "react"
+
+const Error = ()=>{
+	return (
+	<>
+	<h1>Error test 아마 404일걸?</h1>
+	</>
+	);
+}
+export default Error;
---
+++ src/layouts/Login/Login.js
@@ -0,0 +1,11 @@
+import React  from "react"
+// import { Button, Container, Grid, Stack, Divider, Text } from '@mui/material';
+
+const Login = () => {
+	return (
+	<>
+	<h1>Login test</h1>
+	</>
+	)
+}
+export default Login
