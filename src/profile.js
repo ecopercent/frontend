d6def3e212c17822d54f9ec4cf0669b806a7cf72
sumnii <sumsong@student.42seoul.kr>
2023-02-28 15:04:25 +0900

    feat: 하위 컴포넌트들에 edit 상태에 따른 리턴값 추가
    - What & How
    	- 프로필 이미지 alt 값 변화 (임시)
    	- 버튼 내용 변경 (프로필편집 / 완료)
    	- 프로필 텍스트 단에 편집중! 내용 추가 (임시)
    - ECOPERCENT-180

--- src/profile.js
+++ src/profile.js
@@ -10,25 +10,44 @@ export default function Profile({ user }) {
   return (
     <>
       <ProfileImage src={user.profile_image} />
-      <ProfileEditButton />
-      <ProfileMessage nickname={user.nickname} text={user.profile_message} />
+      <ProfileEditButton edit={edit} handleEdit={handleEdit} />
+      <ProfileMessage
+        edit={edit}
+        nickname={user.nickname}
+        text={user.profile_message}
+      />
     </>
   );
 }
 
-function ProfileImage({ src }) {
-  return <img src={src} alt="user_profile_image" />;
+function ProfileImage({ edit, src }) {
+  if (edit)
+    return (
+      <img src={src} alt="user_profile_image_edit" width="100" height="100" />
+    );
+  else
+    return <img src={src} alt="user_profile_image" width="100" height="100" />;
 }
 
-function ProfileEditButton() {
-  return <button>프로필 편집</button>;
+function ProfileEditButton({ edit, handleEdit }) {
+  if (edit) return <button onClick={handleEdit}>완료</button>;
+  else return <button onClick={handleEdit}>프로필 편집</button>;
 }
 
-function ProfileMessage({ nickname, text }) {
-  return (
-    <>
-      <p>{nickname}</p>
-      <p>{text}</p>
-    </>
-  );
+function ProfileMessage({ edit, nickname, text }) {
+  if (edit)
+    return (
+      <>
+        <p>편집중!</p>
+        <p>{nickname}</p>
+        <p>{text}</p>
+      </>
+    );
+  else
+    return (
+      <>
+        <p>{nickname}</p>
+        <p>{text}</p>
+      </>
+    );
 }
