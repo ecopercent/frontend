4379964ea38dab231cf93b111934a3b3a1395bff
sumnii <sumsong@student.42seoul.kr>
2023-02-28 14:43:07 +0900

    feat: 프로필 이미지 컴포넌트 추가
    - How : user 데이터의 profile_image를 소스로 삽입하여 img태그 컴포넌트 생성
    - ECOPERCENT-180

--- src/profile.js
+++ src/profile.js
@@ -2,9 +2,13 @@
 export default function Profile({ user }) {
   return (
     <>
-      {/* 프로필 이미지 */}
+      <ProfileImage src={user.profile_image} />
       {/* 프로필 편집 버튼 */}
       {/* 텍스트 단락 */}
     </>
   );
+}
+
+function ProfileImage({ src }) {
+  return <img src={src} alt="user_profile_image" />;
 }
