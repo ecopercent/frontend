00b9a2d6cabc5786c8cbb912b3c6a75997631083
hyopark <hyopark@hyoparkui-MacBookAir.local>
2023-03-02 10:45:44 +0900

    fix : .eslintrc.json에 arrow-body-style 추가
    .eslintrc.json에 arrow-body-style 추가, lint 에러 수정

--- src/layouts/Error/Error.jsx
+++ src/layouts/Error/Error.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 const Error = () => {
-  return (
-    <>
-      <h1>Error test 아마 404일걸?</h1>
-    </>
-  );
+  return <h1>Error test 아마 404일걸?</h1>;
 };
 export default Error;
