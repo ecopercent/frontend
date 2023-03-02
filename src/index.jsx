3835203f8c5cedbc919c7d91988c1a0a1e866802
sumnii <sumsong@student.42seoul.kr>
2023-03-02 17:50:32 +0900

    rename: 컴포넌트 관련 디렉토리/파일명 대문자 시작, 디렉토리 구조 변경
    - What
    	- 컴포넌트 관련 디렉토리/파일명 대문자 시작으로 변경
    	- 연결된 import 디렉토리명 수정
    	- Components 디렉토리 src/ 하위로 이동
    ECOPERCENT-204

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./layouts/App/App";
+import App from "./Layouts/App/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
