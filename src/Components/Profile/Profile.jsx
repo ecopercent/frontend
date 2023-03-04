9bd3a95a01761d452c0a29ee2da355b3558fd402
sumnii <sumsong@student.42seoul.kr>
2023-03-04 15:28:31 +0900

    feat: 프로필 편집 취소 버튼 삽입 구현
    - Why : 취소 버튼 추가로 인한 로직 변경
    - What & How:
    	- 관리할 state가 많아지므로 객체형 state user로 통합
    	- GET / POST 기능 고려하여 state 핸들링
    	- 버튼의 submit / button 기능 고려하여 세개의 태그로 분리
    		- 같은 위치에 대체되는 버튼은 display 속성으로 공간 삭제
    		- 편집 시에만 활성화되는 버튼은 visibility 속성으로 공간 유지
    ECOPERCENT-180

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,65 +1,69 @@
 import React, { useState } from "react";
 import "./style.css";
 
-const user = {
-	nickname: "유저닉네임",
-	profileImage: "https://avatars.githubusercontent.com/u/75291932?v=4",
-	profileMessage: "Hello Ecopercent!"
-  };
+// GET
+const initialUser = {
+  nickname: "유저닉네임",
+  profileImage: "https://avatars.githubusercontent.com/u/75291932?v=4",
+  profileMessage: "Hello Ecopercent!",
+};
 
 export default function Profile() {
   const [isEditing, setIsEditing] = useState(false);
-  const [nickname, setNickname] = useState(user.nickname);
-  const [message, setMessage] = useState(user.profileMessage);
+  const [user, setUser] = useState(initialUser);
+  const savedUser = { ...initialUser };
 
   return (
-    <>
-      <ProfileImage src={user.profileImage} />
-      <ProfileEditButton isEditing={isEditing} handleEdit={() => {setIsEditing(!isEditing);}} />
-      <ProfileMessage
-        isEditing={isEditing}
-        nickname={nickname}
-        setNickname={setNickname}
-        message={message}
-        setMessage={setMessage}
-      />
-    </>
+    <div>
+      <img src={user.profileImage} alt="User Profile" />
+      <div>
+        {isEditing ? (
+          <form>
+            <input
+              value={user.nickname}
+              onChange={(e) => {
+                setUser({ ...user, nickname: e.target.value });
+              }}
+            />
+            <input
+              value={user.profileMessage}
+              onChange={(e) => {
+                setUser({ ...user, profileMessage: e.target.value });
+              }}
+            />
+          </form>
+        ) : (
+          <>
+            <p>{user.nickname}</p>
+            <p>{user.profileMessage}</p>
+          </>
+        )}
+      </div>
+      <button
+        type="button"
+        className={`btn-edit ${  isEditing ? "btn-replace" : ""}`}
+        onClick={() => {
+          setIsEditing(!isEditing);
+        }}
+      >
+        프로필 편집
+      </button>
+	  <button
+	  	type="submit" className={`btn-edit ${  isEditing ? "" : "btn-replace"}`} onClick={() => 
+	  {
+		setIsEditing(!isEditing)
+		// POST
+	  }}>완료</button>
+      <button
+        type="button"
+        className={`btn-cancel ${isEditing ? "" : "hidden"}`}
+        onClick={() => {
+          setIsEditing(!isEditing);
+          setUser(savedUser);
+        }}
+      >
+        취소
+      </button>
+    </div>
   );
-}
-
-function ProfileImage({ isEditing, src }) {
-    return (
-      <img className="profile-image" src={src} alt={isEditing ? "user_profileImage_edit" : "user_profileImage"} />
-    );
-}
-
-function ProfileEditButton({ isEditing, handleEdit }) {
-  return <button onClick={handleEdit}>{isEditing ? "완료" : "프로필 편집"}</button>;
-}
-
-function ProfileMessage({ isEditing, nickname, setNickname, message, setMessage }) {
-  if (isEditing)
-    return (
-      <form>
-        <input
-          type="text"
-          minLength="1"
-          maxLength="8"
-          value={nickname}
-          onChange={(e) => {setNickname(e.target.value);}}
-        />
-        <input
-          type="text"
-          maxLength="30"
-          value={message}
-          onChange={(e) => {setMessage(e.target.value);}}
-        />
-      </form>
-    );
-return (
-	<>
-	<p>{nickname}</p>
-	<p>{message}</p>
-	</>
-);
 }
