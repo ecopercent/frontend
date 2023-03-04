1b4bcb2135e8d0761e6b6e08ec496ffb4edfb746
sumnii <sumsong@student.42seoul.kr>
2023-03-04 15:49:46 +0900

    refact: 프로필 텍스트, 버튼 컴포넌트화
    - 텍스트(닉네임, 메시지)단, 편집 관련 버튼 컴포넌트화
    ECOPERCENT-180

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -15,45 +15,70 @@ export default function Profile() {
 
   return (
     <div>
-      <img src={user.profileImage} alt="User Profile" />
       <div>
-        {isEditing ? (
-          <form>
-            <input
-              value={user.nickname}
-              onChange={(e) => {
-                setUser({ ...user, nickname: e.target.value });
-              }}
-            />
-            <input
-              value={user.profileMessage}
-              onChange={(e) => {
-                setUser({ ...user, profileMessage: e.target.value });
-              }}
-            />
-          </form>
-        ) : (
-          <>
-            <p>{user.nickname}</p>
-            <p>{user.profileMessage}</p>
-          </>
-        )}
+        <img src={user.profileImage} alt="User profile" />
+        <ProfileText isEditing={isEditing} setUser={setUser} user={user} />
       </div>
+      <ProfileBtns
+        isEditing={isEditing}
+        setIsEditing={setIsEditing}
+        setUser={setUser}
+        savedUser={savedUser}
+      />
+    </div>
+  );
+}
+
+function ProfileText({ isEditing, setUser, user }) {
+  return (
+    <div>
+      {isEditing ? (
+        <form>
+          <input
+            value={user.nickname}
+            onChange={(e) => {
+              setUser({ ...user, nickname: e.target.value });
+            }}
+          />
+          <input
+            value={user.profileMessage}
+            onChange={(e) => {
+              setUser({ ...user, profileMessage: e.target.value });
+            }}
+          />
+        </form>
+      ) : (
+        <>
+          <p>{user.nickname}</p>
+          <p>{user.profileMessage}</p>
+        </>
+      )}
+    </div>
+  );
+}
+
+function ProfileBtns({ isEditing, setIsEditing, setUser, savedUser }) {
+  return (
+    <div>
       <button
         type="button"
-        className={`btn-edit ${  isEditing ? "btn-replace" : ""}`}
+        className={`btn-edit ${isEditing ? "btn-replace" : ""}`}
         onClick={() => {
           setIsEditing(!isEditing);
         }}
       >
         프로필 편집
       </button>
-	  <button
-	  	type="submit" className={`btn-edit ${  isEditing ? "" : "btn-replace"}`} onClick={() => 
-	  {
-		setIsEditing(!isEditing)
-		// POST
-	  }}>완료</button>
+      <button
+        type="submit"
+        className={`btn-edit ${isEditing ? "" : "btn-replace"}`}
+        onClick={() => {
+          setIsEditing(!isEditing);
+          // POST
+        }}
+      >
+        완료
+      </button>
       <button
         type="button"
         className={`btn-cancel ${isEditing ? "" : "hidden"}`}
