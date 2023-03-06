b608016335e52e5c3779f5eb37f6e84405395a51
hyopark <hyopark@hyoparkui-MacBookAir.local>
2023-03-06 13:13:22 +0900

    feat: 하단 탭 구성, 스타일 추가
    개요
    - Home 레이아웃 전반에서 쓰일 탭 컴포넌트  구현
    - Home에서 ConvertNumberToPage 함수로 탭넘버에 맞는 화면 랜더
    컴포넌트 설명
    - FooterWrap 스타일 컴포넌트: 화면 하단에 고정된 사이즈로 구성시켜줌
    - TabBarContainer 스타일 컴포넌트: 탭 바 크기를 조절해줌
    - TabBar :  TabBarContainer 안에 TabItem들을 넣어 구성
    (클릭시 현재 탭 아이템 넘버를 핸들링해줄 setCurrTabNumber,
    현재 탭 넘버를 확인하여 스타일바꿔줄 currTabNumber를 props로 가짐)
    TabItemBackGround 스타일 컴포넌트 : 클릭 핸들러와 현재탭 배경색 변경해줌
    - TabItem : 받아온 아이콘을TabItemBackGround 감싸서 구현
    (해당 탭이 몇번째 아이템인지 정해줄 itemNumber,
    해당 탭에 띄울 아이콘 IconComponent,
    TabBar에서 가져온 setCurrTabNumber, currTabNumber를 props로 가짐)
    
    ECOPERCENT-203

--- src/Layouts/Home/Home.jsx
+++ src/Layouts/Home/Home.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-import Profile from "../../Components/Profile/Profile"
+import React, { useState, useCallback } from "react";
+import TabBar from "../../Components/TabBar/TabBar";
+import ItemPage from "../../pages/ItemPage/ItemPage";
+import MainPage from "../../pages/MainPage/MainPage";
+import SettingPage from "../../pages/SettingPage/SettingPage";
+import Error from "../Error/Error";
+import { PageWrap, FooterWrap } from "./style";
 
 const Home = () => {
-  return <Profile />;
+  const [currTabNumber, setCurrTabNumber] = useState(1);
+  const ConvertNumberToPage = useCallback(() => {
+    switch (Number(currTabNumber)) {
+      case 0:
+        return <SettingPage />;
+      case 1:
+        return <MainPage />;
+      case 2:
+        return <ItemPage />;
+      default:
+        return <Error />;
+    }
+  }, [currTabNumber]);
+
+  return (
+    <div>
+      <PageWrap>{ConvertNumberToPage()}</PageWrap>
+      <FooterWrap>
+        <TabBar
+          setCurrTabNumber={setCurrTabNumber}
+          currTabNumber={currTabNumber}
+        />
+      </FooterWrap>
+    </div>
+  );
 };
+
 export default Home;
---
+++ src/Pages/SettingPage/SettingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SettingPage = () => {
+  return (
+    <div>
+      <h1>세팅페이지</h1>
+    </div>
+  );
+};
+
+export default SettingPage;
