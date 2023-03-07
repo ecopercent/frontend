ae753a68d66e272062bb0d19600a2a72f2e98b79
hyopark <hyopark@hyoparkui-MacBookAir.local>
2023-03-07 18:58:56 +0900

    fix: routeInfo 함수에서 jsx로 수정
    - routeInfo에서 함수로 컴포넌트 뿌리니까 최상단 hook규칙에 어긋나서 수정
    - 추가로 Home page 이름이 Main으로 되있어서 수정 + 작업하던거 올라감
    - 이상한 줄바꿈 수정

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "../../Components/Profile/Profile";
+import TileItem from "../../Components/TitleImg/TitleImg";
 
-const Main = () => {
+const Home = () => {
+  const [showType, setShowType] = useState(0);
+  console.log(showType);
+  console.log(showType);
+  console.log(showType);
+  console.log(showType);
+  const clickHandler = (e) => {
+    console.log(showType);
+    setShowType(e.target.value);
+  };
+
   return (
     <div>
+      <Profile />
+      <button value={0} onClick={clickHandler}>
+        둘다
+      </button>
+      <button value={1} onClick={clickHandler}>
+        에코
+      </button>
+      <button value={2} onClick={clickHandler}>
+        텀블
+      </button>
       <h1>메인페이지</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
+      <TileItem catagory={0} />
+      <TileItem catagory={1} />
+      {/* {converShowType(showType)} */}
     </div>
   );
 };
 
-export default Main;
+export default Home;
