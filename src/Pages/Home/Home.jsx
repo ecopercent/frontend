72e2a0ac0e406937273fa46ea23c62c76fd52ca0
hyopark <hyopark@hyoparkui-MacBookAir.local>
2023-03-09 17:05:28 +0900

    fix: 린트에 맞춰 사용하는 부분 제거

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Profile from "../../Components/Profile/Profile";
 
 const Home = () => {
-  const [showType, setShowType] = useState(0);
-  const clickHandler = (e) => {
-    setShowType(e.target.value);
-  };
-
   return (
     <div>
       <Profile />
-      <button value={0} onClick={clickHandler}>
-        둘다
-      </button>
-      <button value={1} onClick={clickHandler}>
-        에코
-      </button>
-      <button value={2} onClick={clickHandler}>
-        텀블
-      </button>
       <h1>메인페이지</h1>
-      {/* {converShowType(showType)} */}
     </div>
   );
 };
