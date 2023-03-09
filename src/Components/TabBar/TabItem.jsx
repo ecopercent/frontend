d96ff4b917917c0637f926b168989cc5a69f6644
zxcv3508 <37266744+zxcv3508@users.noreply.github.com>
2023-03-09 13:00:57 +0900

    fix: 불필요한 주석 제거
    Co-authored-by: Sumin Song <sumsong@student.42seoul.kr>

--- src/Components/TabBar/TabItem.jsx
+++ src/Components/TabBar/TabItem.jsx
@@ -1,5 +1,4 @@
 import React, { useCallback } from "react";
-// import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { TabItemBackGround } from "./style";
 
