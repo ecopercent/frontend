f82cfce615d1ac85f7fb3464d5e879a94c5a3c6f
sumnii <sumsong@student.42seoul.kr>
2023-03-12 02:19:58 +0900

    feat: 저장된 탭 설정값과 메인아이템이 상이할 경우 핸들링
    - Why
    	- 텀블러 탭만 보기로 설정한 이후, 다른 유저로 로그인 혹은 메인아이템에 변동이 생길 경우
    	- 로컬스토리지에 저장된 탭 설정과 현재 메인 아이템의 카테고리를 비교하여 재설정
    	- 굳이 필요할까? 싶긴 한데..유저 3개로 왔다갔다 테스트하다보니 다른 점이 있어 구현해봄
    
    ECOPERCENT-244

--- src/Components/MainItem/MainItemTab.jsx
+++ src/Components/MainItem/MainItemTab.jsx
@@ -10,34 +10,64 @@ export default function MainItemTab({ userId }) {
       return getUser(userId);
     },
   });
-  
+
   const mainTumblerQuery = useQuery({
-	queryKey: ["mainTumbler", userId],
-	queryFn: () => {
-		return getItem(userQuery.data.titleTumblerId)
-	},
-	enabled: !!userQuery.data?.titleTumblerId
+    queryKey: ["mainTumbler", userId],
+    queryFn: () => {
+      return getItem(userQuery.data.titleTumblerId);
+    },
+    enabled: !!userQuery.data?.titleTumblerId,
   });
 
   const mainEcobagQuery = useQuery({
-	queryKey: ["mainEcobag", userId],
-	queryFn: () => {
-		return getItem(userQuery.data.titleEcobagId)
-	},
-	enabled:!!userQuery.data?.titleEcobagId
+    queryKey: ["mainEcobag", userId],
+    queryFn: () => {
+      return getItem(userQuery.data.titleEcobagId);
+    },
+    enabled: !!userQuery.data?.titleEcobagId,
   });
 
   let localSavedSet = localStorage.getItem("mainTabSetting");
-  localSavedSet = localSavedSet ? JSON.parse(localSavedSet) : {tumbler:true, ecobag:true};
+  localSavedSet = localSavedSet
+    ? JSON.parse(localSavedSet)
+    : { tumbler: true, ecobag: true };
   const [itemTab, setItemTab] = useState(localSavedSet);
+
+  if (
+    (mainTumblerQuery.data === undefined && itemTab.tumbler) ||
+    (mainEcobagQuery.data === undefined && itemTab.ecobag)
+  ) {
+    setItemTab({
+      ecobag: mainEcobagQuery.data !== undefined,
+      tumbler: mainTumblerQuery.data !== undefined,
+    });
+    localStorage.setItem(
+      "mainTabSetting",
+      JSON.stringify({
+        ecobag: mainEcobagQuery.data !== undefined,
+        tumbler: mainTumblerQuery.data !== undefined,
+      })
+    );
+  }
 
   return (
     <div>
-      {(mainTumblerQuery?.data || mainEcobagQuery?.data) && <TabButtons hasTumbler={!!mainTumblerQuery?.data} hasEcobag={!!mainEcobagQuery?.data} setItemTab={setItemTab} />}
+      {(mainTumblerQuery?.data || mainEcobagQuery?.data) && (
+        <TabButtons
+          hasTumbler={!!mainTumblerQuery?.data}
+          hasEcobag={!!mainEcobagQuery?.data}
+          setItemTab={setItemTab}
+        />
+      )}
       {/* 테스트용 뷰 */}
-      {itemTab.tumbler && mainTumblerQuery?.data && <div>{mainTumblerQuery.data.nickname}</div>}
-      {itemTab.ecobag && mainEcobagQuery?.data && <div>{mainEcobagQuery.data.nickname}</div>}
-	  {mainTumblerQuery?.data === undefined && mainEcobagQuery?.data === undefined && <div>아이템을 등록하세요.</div>}
+      {itemTab.tumbler && mainTumblerQuery?.data && (
+        <div>{mainTumblerQuery.data.nickname}</div>
+      )}
+      {itemTab.ecobag && mainEcobagQuery?.data && (
+        <div>{mainEcobagQuery.data.nickname}</div>
+      )}
+      {mainTumblerQuery?.data === undefined &&
+        mainEcobagQuery?.data === undefined && <div>아이템을 등록하세요.</div>}
     </div>
   );
 }
@@ -51,39 +81,48 @@ function TabButtons({ hasTumbler, hasEcobag, setItemTab }) {
         <div>
           <button
             type="button"
-			disabled={!hasTumbler || !hasEcobag}
+            disabled={!hasTumbler || !hasEcobag}
             onClick={() => {
               setItemTab({
                 tumbler: true,
                 ecobag: true,
               });
-			  localStorage.setItem("mainTabSetting", JSON.stringify({tumbler:true, ecobag:true}));
+              localStorage.setItem(
+                "mainTabSetting",
+                JSON.stringify({ tumbler: true, ecobag: true })
+              );
             }}
           >
             전체
           </button>
           <button
             type="button"
-			disabled={!hasTumbler}
+            disabled={!hasTumbler}
             onClick={() => {
               setItemTab({
                 tumbler: true,
                 ecobag: false,
               });
-			  localStorage.setItem("mainTabSetting", JSON.stringify({tumbler:true, ecobag:false}));
+              localStorage.setItem(
+                "mainTabSetting",
+                JSON.stringify({ tumbler: true, ecobag: false })
+              );
             }}
           >
             텀블러
           </button>
           <button
             type="button"
-			disabled={!hasEcobag}
+            disabled={!hasEcobag}
             onClick={() => {
               setItemTab({
                 tumbler: false,
                 ecobag: true,
               });
-			  localStorage.setItem("mainTabSetting", JSON.stringify({tumbler:false, ecobag:true}));
+              localStorage.setItem(
+                "mainTabSetting",
+                JSON.stringify({ tumbler: false, ecobag: true })
+              );
             }}
           >
             에코백
