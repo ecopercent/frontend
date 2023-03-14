559b7fcc8383502be80767d4503a9644d84671da
sumnii <sumsong@student.42seoul.kr>
2023-03-14 18:05:19 +0900

    style: 컬러 에셋 스타일 파일 생성
    - 자주 쓰이는 컬러변수 모음 파일 생성
    
    ECOPERCENT-266

---
+++ src/style/color.jsx
@@ -0,0 +1,6 @@
+// import styled fom "@emotion/styled";
+
+export const lightGreen = "#E1EEDD";
+export const lightGray = "#D9D9D9";
+export const ivory = "#FFFBF1";
+export const basicGreen = "#679855";
