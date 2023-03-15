4c1c31e368e40c56443941d7a2bdff5983a67141
hyopark <hyopark@hyoparkui-MacBookAir.local>
2023-03-15 19:13:54 +0900

    fix: 불필요한 패키지, 코드 제거, 조건 일부 수정 feat: MainItemTab에 TitleItem 추가 - MainItemTab에 TitleItem 추가 - TitleItem props 서버에서 패치해온걸로 변경 - ItemShowControlloer 불필요하다고 판단해서 제거 - Home 정리

--- src/Components/TitleItem/TitleItem.jsx
+++ src/Components/TitleItem/TitleItem.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import ItmeImageStroke from "./ItmeImageStroke";
 
-const TitleItem = ({ catagory, imagePath }) => {
-  // 에코백
-  if (catagory === 0) {
+const TitleItem = ({ itemInfo }) => {
+  if (itemInfo.category === "tumbler") {
     return (
       <div>
-        <ItmeImageStroke divideNum={3} imagePath={imagePath} />
+        <ItmeImageStroke divideNum={1} imagePath={itemInfo.image} />
+        <h3>{itemInfo.nickname}</h3>
         <hr />
-        <h1>0%</h1>
-        <h2>현재 0회</h2>
-        <h2>목표 0회</h2>
+        <h1>{itemInfo.usageCount}%</h1>
+        <h2>현재 {itemInfo.usageCount}회</h2>
+        <h2>목표 {itemInfo.type}회</h2>
       </div>
     );
   }
-  // 텀블러
+  // 에코백
   return (
     <div>
-      <ItmeImageStroke divideNum={1} imagePath={imagePath} />
+      <ItmeImageStroke divideNum={3} imagePath={itemInfo.image} />
+      <h3>{itemInfo.nickname}</h3>
       <hr />
-      <h1>0%</h1>
-      <h2>현재 0회</h2>
-      <h2>목표 0회</h2>
+      <h1>{itemInfo.usageCount}%</h1>
+      <h2>현재 {itemInfo.usageCount}회</h2>
+      <h2>목표 {itemInfo.type}회</h2>
     </div>
   );
 };
