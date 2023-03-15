5078bfea7628026c4559ce7cc0b734f571e1b18f
hyopark <hyopark@hyoparkui-MacBookAir.local>
2023-03-15 21:05:05 +0900

    feat: api 패치 데이터로 타이틀아이템 구성, 테두리 부활억제
    - TitleItem를 api로 받아온 데이터로 구성하게 변경
    - TitleItem에서 같은 코드를 if로 분기했던걸 삼항연산으로 divideNum 넘김
    - 테두리 부활하는걸 막기위해 로컬스토리지를 활용
    checkItemUseCount로 로컬 스토리지확인
    날짜가 바뀌면 기본값으로 다시 시작가능하게 구현(테스트는 안해봄)
    - 부활억제기능을 추가하며 StrockInfo를 따로 저장해 map으로 뿌려줌
    map 안에서 사용 횟수 조건으로 display를 none으로 가려버림
    이로인해 useRef부분 삭제
    
    ECOPERCENT-269

--- src/Components/TitleItem/TitleItem.jsx
+++ src/Components/TitleItem/TitleItem.jsx
@@ -2,22 +2,13 @@ import React from "react";
 import ItmeImageStroke from "./ItmeImageStroke";
 
 const TitleItem = ({ itemInfo }) => {
-  if (itemInfo.category === "tumbler") {
-    return (
-      <div>
-        <ItmeImageStroke divideNum={1} imagePath={itemInfo.image} />
-        <h3>{itemInfo.nickname}</h3>
-        <hr />
-        <h1>{itemInfo.usageCount}%</h1>
-        <h2>현재 {itemInfo.usageCount}회</h2>
-        <h2>목표 {itemInfo.type}회</h2>
-      </div>
-    );
-  }
-  // 에코백
   return (
     <div>
-      <ItmeImageStroke divideNum={3} imagePath={itemInfo.image} />
+      <ItmeImageStroke
+        divideNum={itemInfo.category === "tumbler" ? 3 : 1}
+        imagePath={itemInfo.image}
+        id={itemInfo.id}
+      />
       <h3>{itemInfo.nickname}</h3>
       <hr />
       <h1>{itemInfo.usageCount}%</h1>
