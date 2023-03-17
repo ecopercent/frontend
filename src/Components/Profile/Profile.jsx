656f154c5a63a8c655381ab2f2b19c49c42c7b89
sumnii <sumsong@student.42seoul.kr>
2023-03-17 17:35:18 +0900

    refact: 전체 스타일 관련 리팩토링
    - 사용하지 않는 class 및 import 삭제 (예전 바닐라 css)
    - box-sizing:border-box 전역 설정
    - meta 태그 뷰포트 확대/축소 고정
    
    ECOPERCENT-276

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import ProfileViewer from "./ProfileViewer/ProfileViewer";
 import ProfileEditor from "./ProfileEditor/ProfileEditor";
-// import "./style.css";
 
 export default function Profile({ userId }) {
   if (userId === (undefined || null)) return <Navigate to="/" />;
