9c4c62e3d245427c8391de30109df3423152fd08
sumnii <sumsong@student.42seoul.kr>
2023-03-17 13:48:50 +0900

    refact: 메인아이템탭 코드 리팩토링
    - What & How:
    	- 로컬스토리지에 저장할때 key에 유저아이디 함께 삽입 (reference:호바기 코드)
    	- 탭버튼 클릭 시 반복되는 코드 함수화
    	- ItemMixBtn -> BothItemBtn으로 스타일 컴포넌트명 rename
    
    ECOPERCENT-272

--- src/Components/MainItem/MainItemTab.jsx
+++ src/Components/MainItem/MainItemTab.jsx
@@ -31,33 +31,34 @@ export default function MainItemTab({ userId }) {
     enabled: !!userQuery.data?.titleEcobagId,
   });
 
-  let localSavedSet = localStorage.getItem("mainTabSetting");
+  let localSavedSet = localStorage.getItem(`mainSet${userId}`);
   localSavedSet = localSavedSet
     ? JSON.parse(localSavedSet)
     : { tumbler: true, ecobag: true };
   const [itemTab, setItemTab] = useState(localSavedSet);
 
   return (
-	<>
-    <S.TabContainer>
-      {(mainTumblerQuery?.data || mainEcobagQuery?.data) && (
-        <TabButtons
-          hasTumbler={!!mainTumblerQuery?.data}
-          hasEcobag={!!mainEcobagQuery?.data}
-          setItemTab={setItemTab}
-        />
-      )}
-    </S.TabContainer>
-    <TitleItemContainer>
-      {itemTab.tumbler && mainTumblerQuery?.data && (
-        <TitleItem itemInfo={mainTumblerQuery.data} />
-      )}
-      {itemTab.ecobag && mainEcobagQuery?.data && (
-        <TitleItem itemInfo={mainEcobagQuery.data} />
-      )}
-    </TitleItemContainer>
-    {mainTumblerQuery?.data === undefined &&
-      mainEcobagQuery?.data === undefined && <div>아이템을 등록하세요.</div>}
-		</>
+    <>
+      <S.TabContainer>
+        {(mainTumblerQuery?.data || mainEcobagQuery?.data) && (
+          <TabButtons
+            userId={userId}
+            hasTumbler={!!mainTumblerQuery?.data}
+            hasEcobag={!!mainEcobagQuery?.data}
+            setItemTab={setItemTab}
+          />
+        )}
+      </S.TabContainer>
+      <TitleItemContainer>
+        {itemTab.tumbler && mainTumblerQuery?.data && (
+          <TitleItem itemInfo={mainTumblerQuery.data} />
+        )}
+        {itemTab.ecobag && mainEcobagQuery?.data && (
+          <TitleItem itemInfo={mainEcobagQuery.data} />
+        )}
+      </TitleItemContainer>
+      {mainTumblerQuery?.data === undefined &&
+        mainEcobagQuery?.data === undefined && <div>아이템을 등록하세요.</div>}
+    </>
   );
 }
