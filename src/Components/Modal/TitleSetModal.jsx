96c8d6f2d10c0ed6691ace3ea895cf374e8fc997
sumnii <sumsong@student.42seoul.kr>
2023-03-21 16:35:39 +0900

    feat: Portal을 이용한 모달 컴포넌트 DOM 계층 외부로 이동 구현
    - Why:
    	position과 z-index 속성을 이용해 모달을 최상단으로 끌어올리는 것을 구현할 수 있지만,
    	언제나 최상위 노출을 보장할 수 없고, 부모 스타일의 영향을 받을 수 있으므로
    - What & How:
    	- root 태그 하단에 modal div 태그 생성
    	- modal이 생성되면 modal의 하위(DOM 계층 외부)로 이동함으로써 최상위 보장
    
    ECOPERCENT-302

--- src/Components/Modal/TitleSetModal.jsx
+++ src/Components/Modal/TitleSetModal.jsx
@@ -1,28 +1,33 @@
 import React from "react";
+import ModalContainer from "./ModalContainer";
 
 export default function TitleSetModal({ onClose }) {
   return (
-    <div>
+    <ModalContainer>
       <div>
-        <h1>대표아이템 설정</h1>
-        <p>대표 아이템을 변경하시겠습니까?</p>
         <div>
-          <button
-            onClick={() => {
-              return onClose();
-            }}
-          >
-            취소
-          </button>
-          <button
-            onClick={() => {
-              return onClose();
-            }}
-          >
-            확인
-          </button>
+          <h1>대표아이템 설정</h1>
+          <p>대표 아이템을 변경하시겠습니까?</p>
+          <div>
+            <button
+              type="reset"
+              onClick={() => {
+                return onClose();
+              }}
+            >
+              취소
+            </button>
+            <button
+              type="submit"
+              onClick={() => {
+                return onClose();
+              }}
+            >
+              확인
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </ModalContainer>
   );
 }
