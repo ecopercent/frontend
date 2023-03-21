c2158a79f59e5a87eaaafc9dcbb579f561575752
sumnii <sumsong@student.42seoul.kr>
2023-03-21 22:13:27 +0900

    feat: 개별 인포 서버데이터로 렌더링
    - 추후 null / not null 데이터에 따라 리팩토링 필요
    - 목표 횟수 전체 인포와 동일한 이유로 상수 설정
    
    ECOPERCENT-291

--- src/Components/Item/EachInfo.jsx
+++ src/Components/Item/EachInfo.jsx
@@ -1,30 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
+import { getItem } from "../../Api/item";
 import TitleSetModal from "../Modal/TitleSetModal";
 
 export default function EachInfo({ userId, infoItemId }) {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const itemQuery = useQuery({
+    queryKey: [userId, infoItemId],
+    queryFn: () => {
+      return getItem(infoItemId);
+    },
+  });
 
   return (
     <>
-      {modalOpen && (
+      {modalIsOpen && (
         <TitleSetModal
           onClose={() => {
-            setModalOpen(false);
+            setModalIsOpen(false);
           }}
         />
       )}
       <div>
-        <p>
-          아이템닉네임 | 유저아이디:{userId} | 아이템아이디:{infoItemId}
-        </p>
+        <p>{itemQuery.data?.nickname}</p>
         <button
+          type="button"
           onClick={() => {
-            setModalOpen(true);
+            setModalIsOpen(true);
           }}
         >
           대표 설정
         </button>
         <button
+          type="button"
           onClick={() => {
             return alert("아이템 수정 페이지로 이동!");
           }}
@@ -33,12 +41,34 @@ export default function EachInfo({ userId, infoItemId }) {
         </button>
       </div>
       <div>
-        <p>브랜드</p>
-        <p>타입</p>
-        <p>목표횟수</p>
-        <p>구입가</p>
-        <p>구입일</p>
-        <p>사용횟수</p>
+        <p>
+          브랜드
+          <br />
+          타입
+          <br />
+          목표횟수
+        </p>
+        <p>
+          {itemQuery.data?.brand || ""}
+          <br />
+          {itemQuery.data?.type || ""}
+          <br />
+          300회
+        </p>
+        <p>
+          구입가
+          <br />
+          구입일
+          <br />
+          사용횟수
+        </p>
+        <p>
+          {itemQuery.data?.price ? `${itemQuery.data?.price}원` : ""}
+          <br />
+          {itemQuery.data?.purchaseDate || ""}
+          <br />
+          {itemQuery.data?.usageCount}회
+        </p>
       </div>
     </>
   );
