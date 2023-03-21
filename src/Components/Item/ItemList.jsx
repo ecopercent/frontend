70dad55cb8f498278b5d7c77b38ceede13114ce7
sumnii <sumsong@student.42seoul.kr>
2023-03-21 21:46:18 +0900

    refact: 아이템별 중복 코드 삭제 / 컴포넌트화
    - 텀블러/에코백으로 중복되는 컴포넌트 단일화
    - 컴포넌트화 / 파일 분리
      - ItemList -> ItemListBox로 rename
      - ItemListBtns로 리스트 카테고리 정하는 버튼탭바 컴포넌트 분리
      - ItemList로 아이템 나열하는 리스트 컴포넌트 분리
    
    ECOPERCENT-290

--- src/Components/Item/ItemList.jsx
+++ src/Components/Item/ItemList.jsx
@@ -1,83 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getItemList } from "../../Api/item";
 
-export default function ItemList({ userId, setInfoItemId }) {
-  const [itemListOf, setItemListOf] = useState("tumbler");
-  const tumblersQuery = useQuery({
-    queryKey: ["tumblers", userId],
-    queryFn: getItemList(userId, itemListOf),
-  });
-  const ecobagsQuery = useQuery({
-    queryKey: ["ecobags", userId],
-    queryFn: getItemList(userId, itemListOf),
+export default function ItemList({ userId, itemListOf, setInfoItemId }) {
+  const itemListQuery = useQuery({
+    queryKey: [`${itemListOf}s`, userId],
+    queryFn: () => {
+      getItemList(userId, itemListOf);
+    },
   });
 
   return (
-    <>
-      <h3>아이템 리스트 | 유저아이디:{userId}</h3>
-      <button
-        type="button"
+    <ul>
+      <h1>{itemListOf} 리스트</h1>
+      {itemListQuery.data?.map((item) => {
+        return (
+          <li
+            key={item.id}
+            role="presentation"
+            onClick={() => {
+              return setInfoItemId(item.id);
+            }}
+            onKeyDown={() => {
+              return setInfoItemId(item.id);
+            }}
+          >
+            <img src={item.image} alt={item.nickname} />
+          </li>
+        );
+      })}
+      <li
+        role="presentation"
         onClick={() => {
-          return setItemListOf("tumbler");
+          return alert("추가페이지로 이동!");
+        }}
+        onKeyDown={() => {
+          alert("추가페이지로 이동!");
         }}
       >
-        텀블러
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          return setItemListOf("ecobag");
-        }}
-      >
-        에코백
-      </button>
-      <ul>
-        {itemListOf === "tumbler"
-          ? tumblersQuery.data?.map((tumbler) => {
-              return (
-                <li
-                  key={tumbler.id}
-                  role="presentation"
-                  onClick={() => {
-                    return setInfoItemId(tumbler.id);
-                  }}
-                  onKeyDown={() => {
-                    return setInfoItemId(tumbler.id);
-                  }}
-                >
-                  <img src={tumbler.image} alt={tumbler.nickname} />
-                </li>
-              );
-            })
-          : ecobagsQuery.data?.map((ecobag) => {
-              return (
-                <li
-                  key={ecobag.id}
-                  role="presentation"
-                  onClick={() => {
-                    return setInfoItemId(ecobag.id);
-                  }}
-                  onKeyDown={() => {
-                    return setInfoItemId(ecobag.id);
-                  }}
-                >
-                  <img src={ecobag.image} alt={ecobag.nickname} />
-                </li>
-              );
-            })}
-        <li
-          role="presentation"
-          onClick={() => {
-            return alert("추가페이지로 이동!");
-          }}
-          onKeyDown={() => {
-            alert("추가페이지로 이동!");
-          }}
-        >
-          <img alt="아이템 추가" />
-        </li>
-      </ul>
-    </>
+        <img alt="아이템 추가" />
+      </li>
+    </ul>
   );
 }
