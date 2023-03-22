e67893c2c0975488eed0445278b8bae73d495990
sumnii <sumsong@student.42seoul.kr>
2023-03-22 18:50:18 +0900

    style: 구입일 슬라이스로 포매팅
    - Why : purchasedDate가 원하는 포맷대로 들어오지 않아 문자열 slice로 포매팅
    ECOPERCENT-312

--- src/Components/Item/EachInfo.jsx
+++ src/Components/Item/EachInfo.jsx
@@ -91,7 +91,7 @@ export default function EachInfo({ userId, infoItemId }) {
             <S.InfoValue>
               {itemQuery.data?.price ? `${itemQuery.data?.price}원` : ""}
               <br />
-              {itemQuery.data?.purchaseDate || ""}
+              {itemQuery.data?.purchaseDate.slice(0, 10) || ""}
               <br />
               {itemQuery.data?.usageCount}회
             </S.InfoValue>
