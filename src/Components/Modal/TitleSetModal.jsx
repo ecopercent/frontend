c00c6005e736c92b6c8d370da9efd694df2d5e16
sumnii <sumsong@student.42seoul.kr>
2023-03-22 19:22:12 +0900

    style: 스크롤 방지, 모달 레이아웃 컴포넌트화
    - 모달이 띄워진 동안 body의 overflow 스타일을 hidden 시켜서 스크롤 방지
    - 모달 재사용을 위해 SmallModal로 레이아웃 컴포넌트화
    ECOPERCENT-321

--- src/Components/Modal/TitleSetModal.jsx
+++ src/Components/Modal/TitleSetModal.jsx
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React, { useRef } from "react";
 import { patchTitleItem } from "../../Api/user";
 import useOutsideClick from "../../hooks/useOutsideClick";
-import ModalContainer from "./ModalContainer";
+import SmallModal from "./SmallModal";
 import * as S from "./style";
 
 export default function TitleSetModal({ queryData, onClose }) {
@@ -21,34 +21,30 @@ export default function TitleSetModal({ queryData, onClose }) {
   });
 
   return (
-    <ModalContainer>
-      <S.backgroundOverlay>
-        <S.ModalLayout ref={modalRef}>
-          <S.TextContainer>
-            <S.ModalTitle>대표아이템 설정</S.ModalTitle>
-            <S.ModalContent>대표 아이템을 변경하시겠습니까?</S.ModalContent>
-          </S.TextContainer>
-          <S.BtnContainer>
-            <S.CancelBtn
-              type="button"
-              onClick={() => {
-                return onClose();
-              }}
-            >
-              취소
-            </S.CancelBtn>
-            <S.SubmitBtn
-              type="button"
-              onClick={() => {
-                titleMutation.mutate();
-                onClose();
-              }}
-            >
-              확인
-            </S.SubmitBtn>
-          </S.BtnContainer>
-        </S.ModalLayout>
-      </S.backgroundOverlay>
-    </ModalContainer>
+    <SmallModal onClose={onClose}>
+      <S.TextContainer>
+        <S.ModalTitle>대표아이템 설정</S.ModalTitle>
+        <S.ModalContent>대표 아이템을 변경하시겠습니까?</S.ModalContent>
+      </S.TextContainer>
+      <S.BtnContainer>
+        <S.CancelBtn
+          type="button"
+          onClick={() => {
+            return onClose();
+          }}
+        >
+          취소
+        </S.CancelBtn>
+        <S.SubmitBtn
+          type="button"
+          onClick={() => {
+            titleMutation.mutate();
+            onClose();
+          }}
+        >
+          확인
+        </S.SubmitBtn>
+      </S.BtnContainer>
+    </SmallModal>
   );
 }
