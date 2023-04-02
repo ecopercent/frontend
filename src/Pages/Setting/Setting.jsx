d046f9ea012b17a7524dd5259b945ab1fe01fe7f
zxcv3508 <zxcv3508@navre.com>
2023-04-03 00:25:35 +0900

    feat: 로컬스토리지용 로그아웃, 각 요소 뭐해야할지 콘솔로그로 출력
    - 로그아웃은 메인레이아웃 전체에서 동작하기때문에 메인 레이아웃에서 작업: 유저아이디가 없으면 Naviget로 홈으로 리다이렉트
    - 각각 요소에 뭘할지 적어봄, 의미없고 나중에 저기에도 뭐 띄울지 정해야할듯... 사실 지난 회의에 안건으로 넣엇어야했다!

--- src/Pages/Setting/Setting.jsx
+++ src/Pages/Setting/Setting.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-// import { PcPageWrap } from "../../Layouts/Main/style";
+import { useNavigate } from "react-router-dom";
 import * as S from "./style";
 
 const Setting = () => {
+  
+  const navigate = useNavigate();
   const accountArray = [
     {
       label: "계정 정보",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("계정정보 띄우기");
       },
     },
     {
       label: "로그아웃",
       style: S.Logout,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("로그아웃해주기! 리다이랙트 시키기!");
+        localStorage.removeItem("userId");
+        navigate("/");
       },
     },
   ];
@@ -25,14 +29,14 @@ const Setting = () => {
       label: "???",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("이게 아마 텀블러 에코백 어캐띄울지 설정하려다 만거지?");
       },
     },
     {
       label: "??????????",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("이게 아마 텀블러 에코백 어캐띄울지 설정하려다 만거지?");
       },
     },
   ];
@@ -42,14 +46,14 @@ const Setting = () => {
       label: "개발팀",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("개발팀 정보 띄우기 깃헙 컨플루언스 지라 피그마 등등등");
       },
     },
     {
       label: "문의하기",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("대호 이메일주소");
       },
     },
   ];
@@ -59,18 +63,18 @@ const Setting = () => {
       label: "운영약관",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("우리가 곧 법");
       },
     },
     {
       label: "개인정보처리방침",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("hi");
+        console.log("열씨미 알아보고 알려드리겠습니다. 고객님");
       },
     },
   ];
-  
+
   const elementCount =
     accountArray.length +
     notificationArr.length +
@@ -78,6 +82,7 @@ const Setting = () => {
     infoArray.length;
 
   console.log(elementCount);
+
   return (
     <S.SettingWrap>
       <h1
