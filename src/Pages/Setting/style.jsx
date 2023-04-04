6bd55370f66903b7a47fdaf42d14aadb419ebf9a
zxcv3508 <37266744+zxcv3508@users.noreply.github.com>
2023-04-04 09:13:05 +0900

    Update src/Pages/Setting/style.jsx
    Co-authored-by: Sumin Song <sumsong@student.42seoul.kr>

--- src/Pages/Setting/style.jsx
+++ src/Pages/Setting/style.jsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { basicGreen } from "../../style/color";
 
 export const SettingWrap = styled.div`
   //   border: 1px solid black;
@@ -9,7 +10,7 @@ export const Logout = styled.div`
   padding: ${(prop) => {
     return `${30 / prop.elementCount}%`;
   }};
-  color: green;
+  color: ${basicGreen};
 `;
 
 export const Normal = styled.div`
