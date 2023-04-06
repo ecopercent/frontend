b4412a1e92acdccf68bbba08ea36658916fed67c
zxcv3508 <zxcv3508@navre.com>
2023-04-06 18:25:43 +0900

    feat: 설정페이지 내용수정, 스타일 살짝 변경
    - PM이 작성해준대로상세페이지 수정
    - 스타일 살짝 수정

--- src/Pages/Setting/Setting.jsx
+++ src/Pages/Setting/Setting.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./style";
-import { lightGreen } from "../../style/color";
 
 const Setting = () => {
   const navigate = useNavigate();
   const accountArray = [
     {
-      label: "계정 정보",
+      label: "회원 정보",
       style: S.Normal,
       onClickHandler: () => {
         console.log("계정정보 띄우기");
@@ -22,76 +21,69 @@ const Setting = () => {
         navigate("/");
       },
     },
-  ];
-
-  const notificationArr = [
     {
-      label: "???",
-      style: S.Normal,
+      label: "회원탈퇴",
+      style: S.Logout,
       onClickHandler: () => {
-        console.log("이게 아마 텀블러 에코백 어캐띄울지 설정하려다 만거지?");
-      },
-    },
-    {
-      label: "??????????",
-      style: S.Normal,
-      onClickHandler: () => {
-        console.log("이게 아마 텀블러 에코백 어캐띄울지 설정하려다 만거지?");
+        console.log("탈퇴구질구질쓰");
       },
     },
   ];
 
-  const helpArray = [
+  const appArr = [
+    {
+      label: "앱 소개",
+      style: S.Normal,
+      onClickHandler: () => {
+        console.log("우리 앱 멋지죠?");
+      },
+    },
     {
       label: "개발팀",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("개발팀 정보 띄우기 깃헙 컨플루언스 지라 피그마 등등등");
+        console.log("컨플루언스 페이지로 리디렉션");
       },
     },
     {
-      label: "문의하기",
+      label: "도움말",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("대호 이메일주소");
+        console.log("이거 이렇게 하는겁니다?");
       },
     },
   ];
 
-  const infoArray = [
+  const notificationArr = [
     {
-      label: "운영약관",
+      label: "운영방침",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("우리가 곧 법");
+        console.log("컨플루언스 페이지 리디렉션 혹은 모달뷰로 텍스트만");
       },
     },
     {
       label: "개인정보처리방침",
       style: S.Normal,
       onClickHandler: () => {
-        console.log("열씨미 알아보고 알려드리겠습니다. 고객님");
+        console.log(" 컨플루언스 페이지 리디렉션 모달뷰로 텍스트만");
+      },
+    },
+    {
+      label: "문의처",
+      style: S.Normal,
+      onClickHandler: () => {
+        console.log("컨플루언스 페이지로 리디렉션");
       },
     },
   ];
 
   const elementCount =
-    accountArray.length +
-    notificationArr.length +
-    helpArray.length +
-    infoArray.length;
+    accountArray.length + notificationArr.length + appArr.length;
 
   return (
     <S.SettingWrap>
-      <h1
-        style={{
-          backgroundColor: `${lightGreen}`,
-          // border: "1px solid black",
-          borderRadius: "10px",
-        }}
-      >
-        설정
-      </h1>
+      <S.SettingTitle>설정</S.SettingTitle>
       <S.Category>계정</S.Category>
       {accountArray.map((element) => {
         return (
@@ -105,35 +97,21 @@ const Setting = () => {
         );
       })}
       <hr />
-      <S.Category>알림</S.Category>
-      알림은 어쩌구 입니다.
+      <S.Category>앱</S.Category>
+      {appArr.map((element) => {
+        return (
+          <element.style
+            elementCount={elementCount}
+            key={element.label}
+            onClick={element.onClickHandler}
+          >
+            {element.label}
+          </element.style>
+        );
+      })}
+      <hr />
+      <S.Category>안내</S.Category>
       {notificationArr.map((element) => {
-        return (
-          <element.style
-            elementCount={elementCount}
-            key={element.label}
-            onClick={element.onClickHandler}
-          >
-            {element.label}
-          </element.style>
-        );
-      })}
-      <hr />
-      <S.Category>도움말</S.Category>
-      {helpArray.map((element) => {
-        return (
-          <element.style
-            elementCount={elementCount}
-            key={element.label}
-            onClick={element.onClickHandler}
-          >
-            {element.label}
-          </element.style>
-        );
-      })}
-      <hr />
-      <S.Category>도움말</S.Category>
-      {infoArray.map((element) => {
         return (
           <element.style
             elementCount={elementCount}
--- src/Pages/Setting/style.jsx
+++ src/Pages/Setting/style.jsx
@@ -1,9 +1,21 @@
 import styled from "@emotion/styled";
-import { basicGreen } from "../../style/color";
+import { basicGreen, lightGreen } from "../../style/color";
 
 export const SettingWrap = styled.div`
   //   border: 1px solid black;
   border-radius: 10px;
+  margin-top: 10%;
+`;
+
+export const SettingTitle = styled.div`
+  background: ${lightGreen};
+  border-radius: 5px;
+  padding-left: 2%;
+  padding-bottom: 2.5%;
+  padding-top: 2.5%;
+  margin-bottom: 8%;
+  font-size: 22px;
+  font-weight: bold;
 `;
 
 export const Logout = styled.div`
@@ -11,12 +23,24 @@ export const Logout = styled.div`
     return `${30 / prop.elementCount}%`;
   }};
   color: ${basicGreen};
+  :hover {
+    background: ${basicGreen};
+    color: white;
+  }
 `;
 
 export const Normal = styled.div`
   padding: ${(prop) => {
     return `${30 / prop.elementCount}%`;
   }};
+  :hover {
+    background: ${basicGreen};
+    color: white;
+  }
 `;
 
-export const Category = styled.div``;
+export const Category = styled.div`
+  padding-left: 2%;
+  font-size: 20px;
+  font-weight: bold;
+`;
