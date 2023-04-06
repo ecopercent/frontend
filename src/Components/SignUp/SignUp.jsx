02fb04e52ea8a3c645551938c9f36ae547b85934
sumnii <sumsong@student.42seoul.kr>
2023-04-06 13:00:59 +0900

    feat: 회원가입 페이지 벗어나면 데이터 초기화
    - Why:
      - 취소버튼에만 아이템 초기화를 연동히면, 단순 뒤로가기가 되었을 때 아이템이 남아있어
        로직 흐름이 예상을 벗어남
      - 로그인 페이지에서 아이템 항상 초기화하도록 구현
      - 뒤로가기 감지 이벤트 필요할듯
    ECOPERCENT-394

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -11,13 +11,13 @@ export default function SignUp() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   // const [cancelConfirm, setCancelConfirm] = useState(false);
 
-  const onSubmit = () => {
+  const handleSubmit = () => {
     // TODO: 입력 유효성 검사
     // TODO: 가입 완료 페이지 구현 + 리다이렉트
     return alert("입력 유효성 검사 > 가입 완료 페이지로..");
   };
 
-  const onCancel = () => {
+  const handleClick = () => {
     setModalIsOpen(true);
   };
 
@@ -41,8 +41,8 @@ export default function SignUp() {
           <SignUpItems category="ecobag" />
         </S.InputList>
         <S.SubmitBtnsBox>
-          <S.Btn onClick={onCancel}>취소</S.Btn>
-          <S.Btn featured onClick={onSubmit}>
+          <S.Btn onClick={handleClick}>취소</S.Btn>
+          <S.Btn featured onClick={handleSubmit}>
             등록
           </S.Btn>
         </S.SubmitBtnsBox>
