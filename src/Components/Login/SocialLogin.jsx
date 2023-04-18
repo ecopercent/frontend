321cf5565dbe544e18b507bcee67f3bfed162361
sumnii <sumsong@student.42seoul.kr>
2023-04-18 21:03:49 +0900

    feat: EP 서버에 카카오 액세스 토큰 보내는 것까지 구현
    ECOPERCENT-413

--- src/Components/Login/SocialLogin.jsx
+++ src/Components/Login/SocialLogin.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import kakaoLogin from "./kakaoLogin";
+import { useLocation, useNavigate } from "react-router-dom";
+import { kakaoLogin, getKakaoToken } from "./kakaoLogin";
 import * as S from "./style";
 
 export default function SocialLogin() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (location.pathname.split("/")[2] === "kakao") {
       const params = new URLSearchParams(location.search);
       if (params.has("code")) {
-        console.log("카카오 코드 받았어!");
+        console.log("카카오 인가 코드 받았어!");
         console.log(params.get("code"));
+        getKakaoToken({ authCode: params.get("code"), navigate });
       }
     }
   }, []);
@@ -22,65 +24,3 @@ export default function SocialLogin() {
     </S.LoginButton>
   );
 }
-
-// 효박이의 예제 코드
-
-// export default function SocialLogin() {
-//   const navigate = useNavigate();
-//   const queryParams = new URLSearchParams(window.location.search);
-//   const code = queryParams.get("code");
-//   console.log(code);
-//   useEffect(() => {
-//     if (code) {
-//       console.log("gogo", code);
-//       axios
-//         .post(
-//           `https://kauth.kakao.com/oauth/token`,
-//           {
-//             grant_type: "authorization_code",
-//             client_id: `${process.env.REACT_APP_KAKAO_JS_KEY}`,
-//             redirect_uri: `${process.env.REACT_APP_KAKAO_REDIRECT_URI}`,
-//             code: `${code}`,
-//           },
-//           {
-//             headers: {
-//               "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-//             },
-//           }
-//         )
-//         .then((kakaoAuthResult) => {
-//           console.log(kakaoAuthResult.data);
-//           console.log(
-//             "여기 kakaoAuthResult.data에 엑세스 들어있습니다~ 엑세스토큰으로 then 안에서 아래처럼 다시 우리 서버에 axios 날리면 돼!"
-//           );
-//           axios
-//             .get(`/login/oauth2/kakao`, {
-//               headers: {
-//                 Authorization: `Bearer ${
-//                   kakaoAuthResult.data.access_token ?? "오잉 왜 토큰이 없지"
-//                 }`,
-//               },
-//             })
-//             .then((ecopercentAuthResult) => {
-//               if (ecopercentAuthResult.status === 200) {
-//                 console.log("쿠키에 있는 우리서버 엑세스 저장 후 메인가기");
-//                 navigate("/home");
-//               } else if (ecopercentAuthResult.status === 404) {
-//                 console.log("회원가입 하러 가기");
-//                 navigate("/signup");
-//               }
-//             })
-//             .catch((e) => {
-//               console.log("ecopercent kakao auth erro", e);
-//             });
-//         });
-//       console.log("gogo!!");
-//     }
-//   }, [code]);
-
-//   return (
-//     <S.LoginButton onClick={kakaoLogin}>
-//       <S.LogoImg src="/img/kakaoLogo.png" alt="kakao login" />
-//     </S.LoginButton>
-//   );
-// }
--- src/Components/Login/kakaoLogin.jsx
+++ src/Components/Login/kakaoLogin.jsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import { scriptLoad } from "../../Utils/script";
 
-export default async function kakaoLogin() {
+export async function kakaoLogin() {
   const name = "KAKAO";
   const src = "https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js";
   const crossOrigin = "anonymous";
@@ -17,4 +18,57 @@ export default async function kakaoLogin() {
   }
 
   return null;
+}
+
+async function postKakaoToken({ kakaoAccessToken, navigate }) {
+  try {
+    console.log("에코 서버에 카카오 액세스 토큰 보내기!");
+    const response = await axios.get("/login/oauth2/kakao", {
+      headers: {
+        Authorization: `Bearer ${kakaoAccessToken}`,
+      },
+    });
+    if (response.status === 200) {
+      console.log("유저 있음! 로그인 시켜주자");
+    }
+  } catch (err) {
+    if (err.response.status === 404) {
+      console.log(err);
+      console.log("유저 없음! 회원가입 시켜주자");
+    } else navigate("/");
+  }
+}
+
+export async function getKakaoToken({ authCode, navigate }) {
+  try {
+    const response = await axios.post(
+      "https://kauth.kakao.com/oauth/token",
+      {
+        // data
+        grant_type: "authorization_code",
+        client_id: `${process.env.REACT_APP_KAKAO_JS_KEY}`,
+        redirect_uri: `${process.env.REACT_APP_KAKAO_REDIRECT_URI}`,
+        code: `${authCode}`,
+      },
+      {
+        // config
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      }
+    );
+    if (response.status === 200) {
+      console.log("인가 코드 주고 받아온거!");
+      console.log(response.data);
+      postKakaoToken({
+        kakaoAccessToken: response.data.access_token,
+        navigate,
+      });
+    } else {
+      navigate("/");
+    }
+  } catch (err) {
+    console.log(err);
+    navigate("/");
+  }
 }
