906ae921cfc3f927af89c9ede84b8d71221162b3
sumnii <sumsong@student.42seoul.kr>
2023-04-19 23:54:04 +0900

    feat: 액세스 토큰 메모리에 담고 쿠키에서 삭제
    ECOPERCENT-423

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import cookie from "react-cookies";
-import axios from "axios";
+// import axios from "axios";
 import Profile from "../../Components/Home/Profile/Profile";
 import TitleItemBox from "../../Components/Home/TitleItems/TitleItemBox";
+import { setAccess } from "../../Api/jwt";
 
 const Home = () => {
   useEffect(() => {
     if (cookie.load("access")) {
-      axios.defaults.headers.common.Authorization = `Bearer ${cookie.load(
-        "access"
-      )}`;
+      setAccess(cookie.load("access"));
+      cookie.remove("access");
+      // axios.defaults.headers.common.Authorization = `Bearer ${cookie.load(
+      // "access"
+      // )}`;
     }
   }, []);
 
