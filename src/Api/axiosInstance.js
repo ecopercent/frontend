43ffd483038cfe9fca771b8549c11ed0e1596a3d
sumnii <sumsong@student.42seoul.kr>
2023-04-20 18:11:48 +0900

    feat: 엑세스 토큰 없는 경우 추가
    ECOPERCENT-425

--- src/Api/axiosInstance.js
+++ src/Api/axiosInstance.js
@@ -9,12 +9,15 @@ authInstance.interceptors.request.use(
     const access = getAccess();
 
     if (!access) {
-      config.headers.Authorization = null;
+      // 엑세스 토큰 없을 때 재발급 받아서 넣기
+      // const { data } = await axios.post("/oauth2/access");
+      // config.headers.Authorization = data.;
       return config;
     }
 
     if (config.headers && access) {
       config.headers.Authorization = `Bearer ${access}`;
+      config.withCredentials = true;
       return config;
     }
     return config;
@@ -33,7 +36,13 @@ authInstance.interceptors.response.use(
   },
   // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
   // 응답 오류가 있는 작업 수행
-  (error) => {
+  async (error) => {
+    // 엑세스 토큰 만료
+    if (error.response.status === 401) {
+      // 액세스 토큰 재발급 요청
+      const { data } = await axios.post("/oauth2/access");
+    }
+    // 액세스 토큰 만료
     return Promise.reject(error);
   }
 );
