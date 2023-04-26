c84fbb7e66f472ec512341e38670d4a7ea263f55
zxcv3508 <zxcv3508@navre.com>
2023-04-26 16:37:50 +0900

    feat: 애플로그인 구현
    - 애플에서 제공하는 스크립트로드 후 제공해주는 버튼으로 구현
    - 정확한 동작을 위해서는 clientId, redirectURI를 애플개발자 세팅을 해야함
    - redirectURI를https 도메인으로 설정해야함 (http, 로컬호스트, ip주소 안됌)

---
+++ src/Components/Login/appleLogin.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+
+const AppleLoginButton = () => {
+  const clientId = `${process.env.REACT_APP_APPLE_CLIENT_ID}`;
+  const redirectURI = `${process.env.REACT_APP_APPLE_REDIRECT_URI}`;
+  const scope = "name email";
+  const state = "some-state";
+  const nonce = "some-nonce";
+
+  useEffect(() => {
+    // Apple SDK 스크립트 로드
+    const script = document.createElement("script");
+    script.src =
+      "https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js";
+    script.async = true;
+    script.onload = () => {
+      // Apple SDK 스크립트 로드 후 AppleID.auth.init 함수를 호출하여 버튼을 초기화합니다.
+      window.AppleID.auth.init({
+        clientId,
+        scope,
+        redirectURI,
+        state,
+        nonce,
+      });
+    };
+    document.body.appendChild(script);
+
+    return () => {
+      // 컴포넌트가 unmount될 때, 스크립트를 삭제합니다.
+      document.body.removeChild(script);
+    };
+  }, [clientId, redirectURI, scope, state, nonce]);
+
+  return (
+    <div
+      style={{ width: "55px" }}
+      id="appleid-signin"
+      data-color="black"
+      data-type="sign in"
+      data-mode="logo-only"
+      data-border-radius="50"
+    />
+  );
+};
+
+export default AppleLoginButton;
