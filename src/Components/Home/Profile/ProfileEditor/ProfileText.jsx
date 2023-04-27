1ef601fe770259fa6616d9680ef0fb4b0db57d23
zxcv3508 <zxcv3508@navre.com>
2023-04-27 22:14:54 +0900

    feat: 글자수제한  점검 및 변경
    - 프로필 변경 70자에서 35자로 통일
    - 아이템 추가/변경에서 타입 12자, 구입가 숫자로 캐스팅+12자 제한

--- src/Components/Home/Profile/ProfileEditor/ProfileText.jsx
+++ src/Components/Home/Profile/ProfileEditor/ProfileText.jsx
@@ -9,16 +9,16 @@ export default function ProfileText({ user, setUser, isMobile }) {
         onChange={(e) => {
           setUser({ ...user, nickname: e.target.value });
         }}
-        rows="1"
-        maxLength="8"
+        rows={1}
+        maxLength={8}
       />
       <S.ProfileTextarea
         value={user.profileMessage}
         onChange={(e) => {
           setUser({ ...user, profileMessage: e.target.value });
         }}
-        rows={isMobile ? "4" : "3"}
-        maxLength="70"
+        rows={isMobile ? 4 : 3}
+        maxLength={35}
       />
     </S.ProfileTextForm>
   );
