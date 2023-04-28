804c1c4106c89b382a220f90975060149cc11105
sumnii <sumsong@student.42seoul.kr>
2023-04-28 19:53:48 +0900

    feat: api 호출 성공 시 값에는 url로 변환해서 반환
    ECOPERCENT-446

--- src/Api/user.jsx
+++ src/Api/user.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import b64toBlob from "../Utils/convert";
 
 export function postUserOfKakao(signUpForm) {
   return axios.post("/users/kakao", signUpForm).then((res) => {
@@ -13,26 +14,6 @@ export function postUserOfApple(signUpForm) {
 }
 
 export function getUser() {
-  function b64toBlob(b64Data, contentType) {
-    const sliceSize = 512;
-
-    const byteCharacters = atob(b64Data);
-    const byteArrays = [];
-
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i += 1) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-
-      const byteArray = new Uint8Array(byteNumbers);
-      byteArrays.push(byteArray);
-    }
-    return new File(byteArrays, "pot", { type: contentType });
-  }
-
   return axios.get(`/users/me`).then((res) => {
     if (res.data.profileImage) {
       const file = b64toBlob(res.data.profileImage, "image/png");
@@ -44,6 +25,10 @@ export function getUser() {
 
 export function patchUser(formData) {
   return axios.patch(`/users`, formData).then((res) => {
+    if (res.data.profileImage) {
+      const file = b64toBlob(res.data.profileImage, "image/png");
+      res.data.profileImage = URL.createObjectURL(file);
+    }
     return res.data;
   });
 }
