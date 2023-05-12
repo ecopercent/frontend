816c585454efbb7ffee936a3ae700a7b64b8220e
sumnii <sumsong@student.42seoul.kr>
2023-05-12 16:49:12 +0900

    style: 아이템 추가/수정 페이지 레이아웃 수정
    ECOPERCENT-471

--- src/Pages/ItemModification/style.jsx
+++ src/Pages/ItemModification/style.jsx
@@ -4,37 +4,35 @@ import * as btn from "../../style/button";
 import * as font from "../../style/font";
 
 export const ItemEditWrap = styled.div`
-  width: 500px;
+  width: 100%;
+  max-width: 500px;
+  height: 100%;
   margin: 0 auto;
+  padding: 10px;
 `;
 
 export const ItemEditBorder = styled.div`
   border: 1px solid black;
   border-radius: 10px;
-  margin: 1.5%;
-  height: ${(prop) => {
-    const innerHeight = prop.height * 0.9;
-    const hardHeight = 667;
-    return prop.height < hardHeight
-      ? `${hardHeight * 0.9}px; margin-bottom: ${hardHeight * 0.04}px;`
-      : `${innerHeight}px`;
-  }};
-  ${(prop) => {
-    return prop.width < 360 ? "width: 360px;" : "";
-  }}
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  gap: 10px;
 `;
 
 export const ItemEditHeadWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 3%;
   align-items: center;
+  padding: 15px 20px 0;
 `;
 
 export const ImageClipper = styled.img`
   width: 120px;
   height: 150px;
-  object-fit: cover; /* 이미지 비율 유지 */
+  object-fit: cover;
   border-radius: 45%;
   border: 1px solid;
 `;
@@ -43,86 +41,75 @@ export const ItemImageWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 2%;
-  padding-bottom: 3%;
-  height: 25%;
-`;
-
-export const ItemDetailWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  // flex-direction: column;
-  justify-content: center;
-  // align-items: center;
-  height: 65%;
 `;
 
 export const Form = styled.form`
-  width: 70%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  overflow-y: auto;
 `;
 
 export const FormInnerWrapper = styled.div`
-  width: 100%;
-  height: 80%;
-  padding-top: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  flex: 1 1 auto;
+  overflow-y: auto;
+`;
+
+export const LabelInputSet = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-export const Input = styled.input`
-  height: 10%;
-  width: 80%;
-  font-size: 15px;
-  line-height: 1;
-  padding-left: 5%;
-  border-radius: 6px;
-  --saf-0: rgba(var(--sk_foreground_high_solid, 134, 134, 134), 1);
-  border: 1px solid var(--saf-0);
-  transition: border 80ms ease-out, box-shadow 80ms ease-out;
-  margin-bottom: 5px;
-`;
-
-export const Select = styled.select`
-  height: 10%;
-  width: 80%;
-  font-size: 15px;
-  line-height: 1;
-  padding-left: 5%;
-  border-radius: 6px;
-  --saf-0: rgba(var(--sk_foreground_high_solid, 134, 134, 134), 1);
-  border: 1px solid var(--saf-0);
-  transition: border 80ms ease-out, box-shadow 80ms ease-out;
-  margin-bottom: 5px;
+  gap: 10px;
+  width: 70%;
 `;
 
 export const Span = styled.span`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: baseline;
+  width: 70px;
+  ${font.boldBody};
+`;
+
+export const Input = styled.input`
   width: 80%;
-  padding-right: 10%;
-  margin-bottom: 3px;
+  ${font.normalSmall};
+  line-height: 1;
+  border: 0;
+  border-bottom: 2px solid;
+  -webkit-appearance: none;
+  outline: none;
+  :read-only {
+    border: 0;
+  }
+`;
+
+export const Select = styled.select`
+  font-size: 15px;
+  line-height: 1;
+  padding-left: 5%;
+  border-radius: 6px;
+  --saf-0: rgba(var(--sk_foreground_high_solid, 134, 134, 134), 1);
+  border: 1px solid var(--saf-0);
+  transition: border 80ms ease-out, box-shadow 80ms ease-out;
+  margin-bottom: 5px;
 `;
 
 export const Error = styled.div`
-  font-size: 13px;
+  ${font.normalXsmall};
+  font-weight: 600;
+  text-align: center;
+  margin: auto 0 0;
   color: #e01e5a;
-  padding-bottom: 1%;
-  padding-left: 10%;
   width: 100%;
-  font-weight: bold;
 `;
 
 export const ButtonWrapper = styled.div`
+  width: 100%;
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  height: 10%;
-  width: 80%;
-  margin-left: 10%;
+  padding: 15px 20px;
 `;
 
 export const SubmitBtn = styled.button`
@@ -142,6 +129,8 @@ export const DeleteBtn = styled.button`
 
 export const CameraIcon = styled(HiOutlineCamera)`
   position: absolute;
+  top: 0;
+  left: 0;
   width: 120px;
   height: 150px;
   border-radius: 45%;
