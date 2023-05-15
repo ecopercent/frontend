b57057289f29b9fd637939dc23a034ed15cd0404
sumnii <song.sumin.kr@gmail.com>
2023-05-16 01:17:34 +0900

    feat: 엑세스 토큰 set 함수 내에서 쿠키 접근
    ECOPERCENT-421

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import cookie from "react-cookies";
 import Profile from "../../Components/Home/Profile/Profile";
 import TitleItemBox from "../../Components/Home/TitleItems/TitleItemBox";
 import { setAccessToken } from "../../Api/jwt";
 
 const Home = () => {
-  setAccessToken(cookie.load("access"));
+  setAccessToken();
 
   return (
     <>
