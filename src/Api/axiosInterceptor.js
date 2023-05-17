f391c70ca13fd1c502d55b02c1d59229732b4d07
sumnii <song.sumin.kr@gmail.com>
2023-05-18 08:37:35 +0900

    test: 애플 로그인 테스트 때까지 콘솔 로그 유지
    ECOPERCENT-421

--- src/Api/axiosInterceptor.js
+++ src/Api/axiosInterceptor.js
@@ -25,24 +25,20 @@ axios.interceptors.request.use(
 
 axios.interceptors.response.use(
   (response) => {
-    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
-    // 응답 데이터가 있는 작업 수행
     return response;
   },
-  // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
-  // 응답 오류가 있는 작업 수행
   async (error) => {
     // 엑세스 토큰 만료
     console.log("axios error", error);
     if (error.config.url === "/signout") return Promise.resolve();
     if (error.config.url === "/token/access") {
-      // 로그아웃 처리
+      // TEST: 리프레시 토큰 만료: 로그아웃 처리
       console.log("리프레시 토큰 만료");
       localStorage.setItem("out", true);
       return Promise.resolve();
     }
     if (error.response.status === 401 || error.response.status === 403) {
-      // 액세스 토큰 재발급 요청
+      // TEST: 액세스 토큰 만료
       console.log("엑세스 토큰 만료!", error.response.status);
       await axios.post("/token/access");
       setAccessToken();
