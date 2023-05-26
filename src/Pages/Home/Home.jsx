bccb4332eacf23b66f99df3fdf86376a1d458c3d
sumnii <sumsong@student.42seoul.kr>
2023-05-26 14:36:10 +0900

    fix: Home 컴포넌트에서 엑세스토큰 세팅 함수 호출 삭제
    ECOPERCENT-421

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Profile from "@components/Home/Profile/Profile";
 import TitleItemBox from "@components/Home/TitleItems/TitleItemBox";
-import { setAccessToken } from "@hooks/useAxiosInterceptor";
 
 const Home = () => {
-  setAccessToken();
-
   return (
     <>
       <Profile />
