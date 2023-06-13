c12ad15c2a7ede5d95e8ef76d0dc983a393bf3d2
sumnii <song.sumin.kr@gmail.com>
2023-06-13 16:09:34 +0900

    fix: Authorization Header 재설정하는 axios request config 추가
    ECOPERCENT-482

--- src/hooks/useAxiosInterceptor.js
+++ src/hooks/useAxiosInterceptor.js
@@ -1,30 +1,43 @@
 import axios from "axios";
 import cookie from "react-cookies";
 
+let accessToken = null;
+
 export function setAccessToken() {
-  const accessToken = cookie.load("access", { path: "/" });
-  if (accessToken) {
-    axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+  const newAccessToken = cookie.load("access", { path: "/" });
+  if (newAccessToken) {
+    accessToken = newAccessToken;
     cookie.remove("access", { path: "/" });
   } else console.log("No Access Token from Server");
 }
 
 export function useAxiosInterceptor() {
+  function requestHandler(config) {
+    if (config.headers && accessToken) {
+      const updatedConfig = { ...config };
+      updatedConfig.headers.Authorization = `Bearer ${accessToken}`;
+      return updatedConfig;
+    }
+    return config;
+  }
+
+  axios.interceptors.request.use(requestHandler, (err) => {
+    return Promise.reject(err);
+  });
+
   function responseHandler(response) {
     return response;
   }
 
   async function errorHandler(error) {
     if (error.config.url === "/token/access" && error.response.status !== 500) {
-      console.log("리프레시 토큰 유효하지 않음/만료");
       return Promise.resolve("SIGNOUT");
     }
     if (error.response.status === 403) {
-      console.log("엑세스 토큰 만료!", error.response.status);
       const res = await axios.post("/token/access");
-      console.log("엑세스 토큰 재발급 결과", res);
       if (res === "SIGNOUT") {
-        localStorage.setItem("out", true);
+        // TODO: 다른 방법 생각하기
+        // localStorage.setItem("out", true);
         window.location.replace("/signout");
       }
       setAccessToken();
