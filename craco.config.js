7ed59f45a7de236cafb03d3f0365719c61caf889
sumnii <song.sumin.kr@gmail.com>
2023-06-28 17:56:37 +0900

    fix: craco config 수정

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@api": path.resolve(__dirname, "src/Api"),
+      "@api": path.resolve(__dirname, "src/api"),
       "@components": path.resolve(__dirname, "src/components"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
       "@layout": path.resolve(__dirname, "src/layout"),
