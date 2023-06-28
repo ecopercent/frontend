3d436d77fbb1702502e22a4cfe9c76197ab971ea
zxcv3508 <37266744+zxcv3508@users.noreply.github.com>
2023-06-28 20:44:47 +0900

    Update src/components/modal/DeleteUserModal.jsx
    Co-authored-by: Sumin Song <sumsong@student.42seoul.kr>

--- src/components/modal/DeleteUserModal.jsx
+++ src/components/modal/DeleteUserModal.jsx
@@ -8,7 +8,7 @@ const DeleteUserModal = ({ onClose, onCheckDelete }) => {
       <S.TextContainer>
         <S.ModalTitle>회원탈퇴</S.ModalTitle>
         <S.ModalContent>
-          탈퇴가 된 계정은 복구할 수 없습니다.
+          탈퇴한 계정은 복구할 수 없습니다.
           <br />
           정말 탈퇴하시겠습니까?
         </S.ModalContent>
