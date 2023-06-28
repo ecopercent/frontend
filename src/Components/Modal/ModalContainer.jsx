24477b5e03f672ec7bd9385d398da43b9b858598
sumnii <song.sumin.kr@gmail.com>
2023-06-28 17:59:36 +0900

    rm cached

--- src/Components/Modal/ModalContainer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createPortal } from "react-dom";
-
-export default function ModalContainer({ children }) {
-  return createPortal(<div>{children}</div>, document.getElementById("modal"));
-}
