518cf92087fe61be1721c1384c4ec7bc4a0d66c4
sumnii <song.sumin.kr@gmail.com>
2023-06-28 20:31:18 +0900

    style: 중복스타일 제거, 모바일에서 width 500px 고정되는 부분 조건주어 해제
    ECOPERCENT-488

--- src/layout/style.jsx
+++ src/layout/style.jsx
@@ -13,25 +13,18 @@ export const Layout = styled.div`
   flex-direction: column;
 
   @media ${media.desktop} {
-    height: 100%;
-
     background-color: lightblue;
   }
 
   @media ${media.tabletM} {
-    height: 100%;
-
     background-color: lightcoral;
   }
 
   @media ${media.tabletS} {
-    height: 100%;
-
     background-color: lightgreen;
   }
 
   @media ${media.mobile} {
-    height: 100%;
     height: -webkit-fill-available;
     height: fill-available;
 
@@ -45,11 +38,13 @@ export const OutletWrapper = styled.div`
   flex: 1 0 auto;
   max-height: calc(100% - 48px);
 
-  ${(props) => {
-    if (props.fixedWidth)
-      return `width: 500px; margin: 0 auto; max-height: 100vh;`;
-    return ``;
-  }}
+  @media not ${media.mobile} {
+    ${(props) => {
+      if (props.fixedWidth)
+        return `width: 500px; margin: 0 auto; max-height: 100vh;`;
+      return ``;
+    }}
+  }
 `;
 
 export const FooterWrap = styled.div`
