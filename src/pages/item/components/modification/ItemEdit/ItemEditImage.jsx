4e24a64ae9f946b27373021d6de0b8b155720751
sumnii <sumsong@student.42seoul.kr>
2023-06-30 03:20:27 +0900

    test: 테스트 완료된 콘솔 로그 삭제
    ECOPERCENT-495

--- src/pages/item/components/modification/ItemEdit/ItemEditImage.jsx
+++ src/pages/item/components/modification/ItemEdit/ItemEditImage.jsx
@@ -4,11 +4,8 @@ import * as S from "../style";
 
 const ItemImage = ({ imgFile, setImgFile, category }) => {
   let url = null;
-  if (imgFile && typeof imgFile === "object") url = URL.createObjectURL(imgFile);
-
-  // TEST: 서버 데이터를 받는 경우와, blob 형태의 경우 확인
-  console.log("imgFile 타입", typeof imgFile);
-  console.log("url 타입", typeof url);
+  if (imgFile && typeof imgFile === "object")
+    url = URL.createObjectURL(imgFile);
 
   const Form = useImgInput({
     prevImg: url || imgFile,
