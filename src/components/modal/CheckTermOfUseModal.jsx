51d2015c856fb9e256df5045cce51a31724ebe46
sumnii <song.sumin.kr@gmail.com>
2023-07-05 19:48:24 +0900

    style: (이럴때만)디자이너의 UI/UX 한스푼~!
    ECOPERCENT-502

--- src/components/modal/CheckTermOfUseModal.jsx
+++ src/components/modal/CheckTermOfUseModal.jsx
@@ -34,60 +34,61 @@ const CheckTermOfUseModal = ({ onClose, onSubmit }) => {
     <LargeModal onClose={onClose}>
       <S.TextContainer>
         <S.ModalTitle>환영합니다</S.ModalTitle>
-        <S.ModalTitle>에코퍼센트 이용을 위해</S.ModalTitle>
-        <S.ModalTitle>아래 약관에 동의해주세요.</S.ModalTitle>
+        <S.ModalTitle>
+          에코퍼센트 이용을 위해
+          <br />
+          아래 약관에 동의해주세요.
+        </S.ModalTitle>
       </S.TextContainer>
-      <S.TextContainer>
-        <form>
-          <S.LabelInputSet>
-            <S.CheckBox
-              type="checkbox"
-              checked={allChecked}
-              onChange={onAllCheck}
-            />
-            <S.Plain onClick={onAllCheck}>전체동의</S.Plain>
-          </S.LabelInputSet>
-          <hr style={{ width: "100%" }} />
-          <S.LabelInputSet>
-            <S.CheckBox
-              type="checkbox"
-              checked={termChecked}
-              onChange={() => {
-                setTermChecked(!termChecked);
-              }}
-            />
-            <S.HoverPlain
-              onClick={() => {
-                window.open(
-                  "https://ecopercent.notion.site/962969ee318545cf9dc2b70b5067fcb2",
-                  "_blank"
-                );
-              }}
-            >
-              이용약관 동의 (필수)
-            </S.HoverPlain>
-          </S.LabelInputSet>
-          <S.LabelInputSet>
-            <S.CheckBox
-              type="checkbox"
-              checked={agreeChecked}
-              onChange={() => {
-                setAgreeChecked(!agreeChecked);
-              }}
-            />
-            <S.HoverPlain
-              onClick={() => {
-                window.open(
-                  "https://ecopercent.notion.site/c7b9f6b4b8894c7881dbb846d26ec15f",
-                  "_blank"
-                );
-              }}
-            >
-              개인정보 수집 및 이용 동의 (필수)
-            </S.HoverPlain>
-          </S.LabelInputSet>
-        </form>
-      </S.TextContainer>
+      <S.TermsForm>
+        <S.LabelInputSet>
+          <S.CheckBox
+            type="checkbox"
+            checked={allChecked}
+            onChange={onAllCheck}
+          />
+          <S.Plain onClick={onAllCheck}>전체동의</S.Plain>
+        </S.LabelInputSet>
+        <hr style={{ width: "100%" }} />
+        <S.LabelInputSet>
+          <S.CheckBox
+            type="checkbox"
+            checked={termChecked}
+            onChange={() => {
+              setTermChecked(!termChecked);
+            }}
+          />
+          <S.HoverPlain
+            onClick={() => {
+              window.open(
+                "https://ecopercent.notion.site/962969ee318545cf9dc2b70b5067fcb2",
+                "_blank"
+              );
+            }}
+          >
+            이용약관 동의 (필수)
+          </S.HoverPlain>
+        </S.LabelInputSet>
+        <S.LabelInputSet>
+          <S.CheckBox
+            type="checkbox"
+            checked={agreeChecked}
+            onChange={() => {
+              setAgreeChecked(!agreeChecked);
+            }}
+          />
+          <S.HoverPlain
+            onClick={() => {
+              window.open(
+                "https://ecopercent.notion.site/c7b9f6b4b8894c7881dbb846d26ec15f",
+                "_blank"
+              );
+            }}
+          >
+            개인정보 수집 및 이용 동의 (필수)
+          </S.HoverPlain>
+        </S.LabelInputSet>
+      </S.TermsForm>
       <S.ErrorContainer>
         {!validatedCheck && (
           <S.ErrorText>필수 항목을 모두 동의해주세요</S.ErrorText>
