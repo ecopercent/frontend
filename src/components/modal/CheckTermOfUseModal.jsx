21023e45a5e706a93c71bf3ee6d9560f70401450
zxcv3508 <zxcv3508@navre.com>
2023-07-05 15:31:59 +0900

    fix:회원가입시 동의 부분  노션페이지 새 창 열기 방식으로 변경

--- src/components/modal/CheckTermOfUseModal.jsx
+++ src/components/modal/CheckTermOfUseModal.jsx
@@ -3,177 +3,104 @@ import LargeModal from "./LargeModal";
 import * as S from "./style";
 
 const CheckTermOfUseModal = ({ onClose, onSubmit }) => {
-  const [showType, setShowType] = useState("");
   const [allChecked, setAllChecked] = useState(false);
   const [termChecked, setTermChecked] = useState(false);
   const [agreeChecked, setAgreeChecked] = useState(false);
-  const [privacyChecked, setPrivacyChecked] = useState(false);
+  // const [privacyChecked, setPrivacyChecked] = useState(false);
   const [validatedCheck, setValidatedCheck] = useState(false);
   const validateCheck = useCallback(() => {
-    if (termChecked && agreeChecked && privacyChecked) onSubmit();
+    if (termChecked && agreeChecked) onSubmit();
     else setValidatedCheck(false);
-  }, [termChecked, privacyChecked, agreeChecked]);
+  }, [termChecked, agreeChecked]);
   useEffect(() => {
     setValidatedCheck(true);
-    if (termChecked && privacyChecked && agreeChecked) setAllChecked(true);
+    if (termChecked && agreeChecked) setAllChecked(true);
     else setAllChecked(false);
-  }, [termChecked, privacyChecked, agreeChecked]);
+  }, [termChecked, agreeChecked]);
   const onAllCheck = useCallback(() => {
     if (!allChecked) {
       setTermChecked(true);
-      setPrivacyChecked(true);
+
       setAgreeChecked(true);
       setAllChecked(!allChecked);
     } else {
       setAllChecked(!allChecked);
       setTermChecked(false);
-      setPrivacyChecked(false);
       setAgreeChecked(false);
     }
   }, [allChecked]);
 
-  switch (showType) {
-    case "term":
-      return (
-        <LargeModal onClose={onClose}>
-          <S.HoverPlain
-            onClick={() => {
-              setShowType("");
-            }}
-          >
-            {"< 이용약관"}
-          </S.HoverPlain>
-          <iframe
-            style={{ backgroundColor: "white" }}
-            src="/docs/term.html"
-            title="이용약관"
-            height="410"
-            width="320"
-          />
-        </LargeModal>
-      );
-    case "privacy":
-      return (
-        <LargeModal onClose={onClose}>
-          <S.HoverPlain
-            onClick={() => {
-              setShowType("");
-            }}
-          >
-            {"< 개인정보 처리 방침"}
-          </S.HoverPlain>
-          <iframe
-            style={{ backgroundColor: "white" }}
-            src="/docs/privacy.html"
-            title="개인정보 처리 방침"
-            height="410"
-            width="320"
-          />
-        </LargeModal>
-      );
-    case "agree":
-      return (
-        <LargeModal onClose={onClose}>
-          <S.HoverPlain
-            onClick={() => {
-              setShowType("");
-            }}
-          >
-            {"< 개인정보 수집 및 이용"}
-          </S.HoverPlain>
-          <iframe
-            style={{ backgroundColor: "white" }}
-            src="/docs/agree.html"
-            title="개인정보 처리 동의"
-            height="410"
-            width="320"
-          />
-        </LargeModal>
-      );
-    case "":
-    default:
-      return (
-        <LargeModal onClose={onClose}>
-          <S.TextContainer>
-            <S.ModalTitle>환영합니다</S.ModalTitle>
-            <S.ModalTitle>에코퍼센트 이용을 위해</S.ModalTitle>
-            <S.ModalTitle>아래 약관에 동의해주세요.</S.ModalTitle>
-            <form>
-              <S.LabelInputSet>
-                <S.CheckBox
-                  type="checkbox"
-                  checked={allChecked}
-                  onChange={onAllCheck}
-                />
-                <S.Plain onClick={onAllCheck}>전체동의</S.Plain>
-              </S.LabelInputSet>
-              <hr style={{ width: "100%" }} />
-              <S.LabelInputSet>
-                <S.CheckBox
-                  type="checkbox"
-                  checked={termChecked}
-                  onChange={() => {
-                    setTermChecked(!termChecked);
-                  }}
-                />
-                <S.HoverPlain
-                  onClick={() => {
-                    setShowType("term");
-                  }}
-                >
-                  이용약관 동의 (필수)
-                </S.HoverPlain>
-              </S.LabelInputSet>
-              <S.LabelInputSet>
-                <S.CheckBox
-                  type="checkbox"
-                  checked={privacyChecked}
-                  onChange={() => {
-                    setPrivacyChecked(!privacyChecked);
-                  }}
-                />
-                <S.HoverPlain
-                  onClick={() => {
-                    setShowType("privacy");
-                  }}
-                >
-                  개인정보 처리 방침 동의 (필수)
-                </S.HoverPlain>
-              </S.LabelInputSet>
-              <S.LabelInputSet>
-                <S.CheckBox
-                  type="checkbox"
-                  checked={agreeChecked}
-                  onChange={() => {
-                    setAgreeChecked(!agreeChecked);
-                  }}
-                />
-                <S.HoverPlain
-                  onClick={() => {
-                    setShowType("agree");
-                  }}
-                >
-                  개인정보 수집 및 이용 동의 (필수)
-                </S.HoverPlain>
-              </S.LabelInputSet>
-            </form>
-          </S.TextContainer>
-          <S.ErrorContainer>
-            {!validatedCheck && (
-              <S.ErrorText>필수 항목을 모두 동의해주세요</S.ErrorText>
-            )}
-          </S.ErrorContainer>
-          <S.BtnContainer>
-            <S.Btn type="reset" onClick={onClose}>
-              취소
-            </S.Btn>
-            <S.Btn warning onClick={validateCheck}>
-              등록
-            </S.Btn>
-          </S.BtnContainer>
-        </LargeModal>
-      );
-  }
+  return (
+    <LargeModal onClose={onClose}>
+      <S.TextContainer>
+        <S.ModalTitle>환영합니다</S.ModalTitle>
+        <S.ModalTitle>에코퍼센트 이용을 위해</S.ModalTitle>
+        <S.ModalTitle>아래 약관에 동의해주세요.</S.ModalTitle>
+        <form>
+          <S.LabelInputSet>
+            <S.CheckBox
+              type="checkbox"
+              checked={allChecked}
+              onChange={onAllCheck}
+            />
+            <S.Plain onClick={onAllCheck}>전체동의</S.Plain>
+          </S.LabelInputSet>
+          <hr style={{ width: "100%" }} />
+          <S.LabelInputSet>
+            <S.CheckBox
+              type="checkbox"
+              checked={termChecked}
+              onChange={() => {
+                setTermChecked(!termChecked);
+              }}
+            />
+            <S.HoverPlain
+              onClick={() => {
+                window.open(
+                  "https://ecopercent.notion.site/962969ee318545cf9dc2b70b5067fcb2",
+                  "_blank"
+                );
+              }}
+            >
+              이용약관 동의 (필수)
+            </S.HoverPlain>
+          </S.LabelInputSet>
+          <S.LabelInputSet>
+            <S.CheckBox
+              type="checkbox"
+              checked={agreeChecked}
+              onChange={() => {
+                setAgreeChecked(!agreeChecked);
+              }}
+            />
+            <S.HoverPlain
+              onClick={() => {
+                window.open(
+                  "https://ecopercent.notion.site/c7b9f6b4b8894c7881dbb846d26ec15f",
+                  "_blank"
+                );
+              }}
+            >
+              개인정보 수집 및 이용 동의 (필수)
+            </S.HoverPlain>
+          </S.LabelInputSet>
+        </form>
+      </S.TextContainer>
+      <S.ErrorContainer>
+        {!validatedCheck && (
+          <S.ErrorText>필수 항목을 모두 동의해주세요</S.ErrorText>
+        )}
+      </S.ErrorContainer>
+      <S.BtnContainer>
+        <S.Btn type="reset" onClick={onClose}>
+          취소
+        </S.Btn>
+        <S.Btn warning onClick={validateCheck}>
+          등록
+        </S.Btn>
+      </S.BtnContainer>
+    </LargeModal>
+  );
 };
 
 export default CheckTermOfUseModal;
